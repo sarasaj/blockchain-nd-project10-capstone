aed8de67bcfe9379b710d2ef7640c098ff2c50bf
saraAlhasan <sara.sh@outlook.com>
2019-04-23 19:40:40 +0300

    fill  getApproved(uint256 tokenId)

--- Blockchain-Capstone-master/eth-contracts/contracts/ERC721Mintable.sol
+++ Blockchain-Capstone-master/eth-contracts/contracts/ERC721Mintable.sol
@@ -164,6 +164,7 @@ contract ERC721 is Pausable, ERC165 {
 
     function getApproved(uint256 tokenId) public view returns (address) {
         // TODO return token approval if it exists
+        return _tokenApprovals[tokenId];
     }
 
     /**
